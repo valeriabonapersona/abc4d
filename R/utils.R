7a4fc2d8fc98202615e3ad0ea04327b561b295de
Valeria Bonapersona <valeria.bonapersona@gmail.com>
2021-03-18 14:16:59 +0100

    envs changed to recipes

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,7 @@ usethis::use_package('ggplot2') # for visualizations (heatmap)
 usethis::use_package('stringr') # for manipulation to strings
 usethis::use_package('assertthat') # for checks in functions
 usethis::use_package('rmarkdown') # is this necessary for vignettes?
-usethis::use_package('EnvStats') # for boxcox transformation
+usethis::use_package('recipes') # for boxcox transformation
 usethis::use_package('ggrepel') # to repel labels in ggplot visual
 usethis::use_package('ks') # for kernel densities
 usethis::use_package("effsize") # to calculate effect sizes
